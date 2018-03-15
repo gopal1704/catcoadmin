05eed12606e3d1f5344bcd9dcb6428fefd236014
gopal <vgopalooty@gmail.com>
2018-03-15 16:54:28 +0530

    cc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { UseraccountsComponent } from './useraccounts/useraccounts.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { WithdrawalrequestsComponent } from './withdrawalrequests/withdrawalrequests.component';
+import { AddwalletbalanceComponent } from './addwalletbalance/addwalletbalance.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path : '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'login', component :AdminloginComponent },
+  {path: 'dashboard', component :DashboardComponent,
+children :[
+  {path : '', redirectTo: '/dashboard/users', pathMatch: 'full' },
+  { path : 'users' , component : UsersComponent },
+    { path : 'useraccounts' , component : UseraccountsComponent},
+    { path : 'investments' , component :InvestmentsComponent },
+    { path : 'transactions' , component : TransactionsComponent},
+    { path : 'withdrawalrequests' , component : WithdrawalrequestsComponent },
+    { path : 'addwalletbalance' , component : },
+
+
+
+ 
+]
+
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+ }
