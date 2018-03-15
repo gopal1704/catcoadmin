34288eb82918345211b93c2ab696ca1b497920d9
gopal <vgopalooty@gmail.com>
2018-03-15 16:18:07 +0530

    'initial'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { UseraccountsComponent } from './useraccounts/useraccounts.component';
+import { InvestmentsComponent } from './investments/investments.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminloginComponent,
+    DashboardComponent,
+    UsersComponent,
+    UseraccountsComponent,
+    InvestmentsComponent
   ],
   imports: [
     BrowserModule,
