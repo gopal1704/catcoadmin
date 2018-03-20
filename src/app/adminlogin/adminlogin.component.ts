beecc7c09883be0a45fd3a484488a3561561e39b
gopal <vgopalooty@gmail.com>
2018-03-20 22:31:06 +0530

    cc

--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StringLike } from '@firebase/util/dist/esm/src/errors';
+declare var Messenger: any;
 
 @Component({
   selector: 'app-adminlogin',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminlogin.component.css']
 })
 export class AdminloginComponent implements OnInit {
+email : string;
+password : string;
+public loading = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+Login(){
+  this.loading = true;
+if((this.email==="info@catcotrade.info")&&(this.password==="catcotrade2000")){
+  setTimeout(()=> {
+this.loading = false;
+    this.router.navigate(['/dashboard']);
+  }, 2500);
 
+}else{
+  Messenger().post({
+    message: 'Error loggin in : Incorrect Email or Password',
+    type: 'error',
+    showCloseButton: true
+  });
+
+}
+
+
+}
 }
